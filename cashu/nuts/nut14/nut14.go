c075744373d95238ebd2248883aca3ea6d26b826
elnosh <elnosh@pm.me>
2024-10-24 10:43:58 -0500

    create HTLCs from wallet

---
+++ cashu/nuts/nut14/nut14.go
@@ -0,0 +1,50 @@
+package nut14
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+
+	"github.com/btcsuite/btcd/btcec/v2"
+	"github.com/decred/dcrd/dcrec/secp256k1/v4/schnorr"
+	"github.com/elnosh/gonuts/cashu"
+	"github.com/elnosh/gonuts/cashu/nuts/nut10"
+)
+
+type HTLCWitness struct {
+	Preimage   string   `json:"preimage"`
+	Signatures []string `json:"signatures"`
+}
+
+func IsSecretHTLC(proof cashu.Proof) bool {
+	return nut10.SecretType(proof) == nut10.HTLC
+}
+
+func AddWitnessHTLC(
+	proofs cashu.Proofs,
+	preimage string,
+	signingKey *btcec.PrivateKey,
+) (cashu.Proofs, error) {
+	for i, proof := range proofs {
+		hash := sha256.Sum256([]byte(proof.Secret))
+		signature, err := schnorr.Sign(signingKey, hash[:])
+		if err != nil {
+			return nil, err
+		}
+		signatureBytes := signature.Serialize()
+
+		htlcWitness := HTLCWitness{
+			Preimage:   preimage,
+			Signatures: []string{hex.EncodeToString(signatureBytes)},
+		}
+
+		witness, err := json.Marshal(htlcWitness)
+		if err != nil {
+			return nil, err
+		}
+		proof.Witness = string(witness)
+		proofs[i] = proof
+	}
+
+	return proofs, nil
+}
