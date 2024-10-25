dab1f9922c21e9a524bd7ada13bd34bef6b704b9
elnosh <elnosh@pm.me>
2024-10-25 11:40:17 -0500

    receive HTLCs in wallet

--- cashu/nuts/nut14/nut14.go
+++ cashu/nuts/nut14/nut14.go
@@ -4,10 +4,14 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"encoding/json"
+	"errors"
+	"slices"
 
 	"github.com/btcsuite/btcd/btcec/v2"
-	"github.com/decred/dcrd/dcrec/secp256k1/v4/schnorr"
+	"github.com/btcsuite/btcd/btcec/v2/schnorr"
 	"github.com/elnosh/gonuts/cashu"
+	"github.com/elnosh/gonuts/cashu/nuts/nut10"
+	"github.com/elnosh/gonuts/cashu/nuts/nut11"
 )
 
 type HTLCWitness struct {
@@ -15,22 +19,54 @@ type HTLCWitness struct {
 	Signatures []string `json:"signatures"`
 }
 
+// AddWitnessHTLC will add the preimage to the HTLCWitness.
+// It will also read the tags in the secret and add the signatures
+// if needed.
 func AddWitnessHTLC(
 	proofs cashu.Proofs,
+	secret nut10.WellKnownSecret,
 	preimage string,
 	signingKey *btcec.PrivateKey,
 ) (cashu.Proofs, error) {
-	for i, proof := range proofs {
-		hash := sha256.Sum256([]byte(proof.Secret))
-		signature, err := schnorr.Sign(signingKey, hash[:])
-		if err != nil {
-			return nil, err
-		}
-		signatureBytes := signature.Serialize()
+	tags, err := nut11.ParseP2PKTags(secret.Data.Tags)
+	if err != nil {
+		return nil, err
+	}
 
-		htlcWitness := HTLCWitness{
-			Preimage:   preimage,
-			Signatures: []string{hex.EncodeToString(signatureBytes)},
+	signatureNeeded := false
+	publicKey := signingKey.PubKey().SerializeCompressed()
+
+	if tags.NSigs > 0 {
+		// return error if it requires more than 1 signature
+		if tags.NSigs > 1 {
+			return nil, errors.New("unable to provide enough signatures")
+		}
+
+		canSign := false
+		// read pubkeys and check signingKey can sign
+		for _, pk := range tags.Pubkeys {
+			if slices.Equal(pk.SerializeCompressed(), publicKey) {
+				canSign = true
+				break
+			}
+		}
+		if !canSign {
+			return nil, errors.New("signing key is not part of public keys list that can provide signatures")
+		}
+
+		// if it gets to here, signature is needed in the witness
+		signatureNeeded = true
+	}
+
+	for i, proof := range proofs {
+		htlcWitness := HTLCWitness{Preimage: preimage}
+		if signatureNeeded {
+			hash := sha256.Sum256([]byte(proof.Secret))
+			signature, err := schnorr.Sign(signingKey, hash[:])
+			if err != nil {
+				return nil, err
+			}
+			htlcWitness.Signatures = []string{hex.EncodeToString(signature.Serialize())}
 		}
 
 		witness, err := json.Marshal(htlcWitness)
@@ -42,4 +78,32 @@ func AddWitnessHTLC(
 	}
 
 	return proofs, nil
+}
+
+func AddWitnessHTLCToOutputs(
+	outputs cashu.BlindedMessages,
+	preimage string,
+	signingKey *btcec.PrivateKey,
+) (cashu.BlindedMessages, error) {
+	for i, output := range outputs {
+		hash := sha256.Sum256([]byte(output.B_))
+		signature, err := schnorr.Sign(signingKey, hash[:])
+		if err != nil {
+			return nil, err
+		}
+
+		htlcWitness := HTLCWitness{
+			Preimage:   preimage,
+			Signatures: []string{hex.EncodeToString(signature.Serialize())},
+		}
+
+		witness, err := json.Marshal(htlcWitness)
+		if err != nil {
+			return nil, err
+		}
+		output.Witness = string(witness)
+		outputs[i] = output
+	}
+
+	return outputs, nil
 }
